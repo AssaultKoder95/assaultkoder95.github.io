7430512406e60b472b86f823974905284b7fb077
assaultkoder95 <abhishekkhanna1995@gmail.com>
2017-10-23 22:18:49 +0530

    Updated JS & CSS

--- soh_app/js/main.js
+++ soh_app/js/main.js
@@ -1,3 +1,9 @@
+
+/*
+
+Code for index.html
+
+*/
 var showText = function (target, message, index, interval) {    
   if (index < message.length) { 
     $(target).append(message[index++]); 
@@ -11,12 +17,69 @@ $(function () {
  
 }); 
 
+/*
 
-function startProcedure(){
-	var div = document.getElementById('learnWithMe');
-	div.className += " fadeOut";
+Code for app.html
 
-	var newDiv = document.getElementById('startApp');
-	newDiv.className += " fadeIn";
-	newDiv.style.display = "block";
+*/ 
+
+function startApp(){
+
+	var preloader   = document.getElementById('preloader');
+	var application = document.getElementById('startApp');
+
+	setTimeout(function(){
+		preloader.style.display = "none";
+		application.className += " animated fadeIn";
+		application.style.display = "block";
+	},1500);
+	
 }
+
+var answer = ['sel1','sel7','sel10','sel5'];
+var answerCount = 0;
+
+function placeImg (imgId) {
+
+	if( answer.indexOf(imgId) < 0) {
+	var img = document.getElementById(imgId);
+	img.className = " animated fadeOut selectImg";
+	} 
+	else {
+		var img = document.getElementById(imgId);
+		var myimg = img.getElementsByTagName('img')[0];
+		img.className = " animated fadeOut selectImg";	
+		var imgPlace = document.getElementById( answer.indexOf(imgId) );
+		var addImages = "<a> <img class=\"animated fadeIn selectImg\" style=\"margin-top:-50px\" src=\""+myimg.src+"\" > </a> ";
+		imgPlace.innerHTML = addImages;
+		imgPlace.className = "col-md-2";
+		answerCount++;		
+	}
+}
+	
+function checkValue(){
+
+	if(answerCount === 4) {
+		notie.alert({ type: 'success', text: 'Success!', time: 2 });
+	}
+	else {
+		notie.alert({ type: 'error', text: 'Oops!', time: 2 });	
+	}
+}	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
